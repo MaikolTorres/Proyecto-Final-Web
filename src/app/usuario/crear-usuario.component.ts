a2662eb79e274238ad2a2651314e369b25d9a640
Damian2340842 <damianparedes842@gmail.com>
2024-01-26 01:12:54 -0500

    aaa
    aaaa

--- src/app/usuario/crear-usuario.component.ts
+++ src/app/usuario/crear-usuario.component.ts
@@ -21,7 +21,6 @@ export class CrearUsuarioComponent implements OnInit {
   usuario1: Usuario[] = [];
   persona1: Persona[] = [];
   roles: Rol[] = [];
-  personas: Persona[] = [];
   isLoading: boolean = true;
   nuevoUsu: Usuario = new Usuario();
   botonDesactivado: boolean = false;
@@ -32,7 +31,10 @@ export class CrearUsuarioComponent implements OnInit {
     private usuarioService: UsuarioService,
     private http: HttpClient,
     private router: Router
-  ) {}
+  ) {
+
+
+  }
 
   ngOnInit() {
     this.createForm();
@@ -48,7 +50,7 @@ export class CrearUsuarioComponent implements OnInit {
 
 // Cargar personas al inicializar el componente
   cargarPersonas() {
-    this.getPersonas().subscribe(personas => (this.personas = personas));
+    this.getPersonas().subscribe(personas => (this.persona1 = personas));
   }
 
   getRoles(): Observable<Rol[]> {
@@ -99,10 +101,12 @@ export class CrearUsuarioComponent implements OnInit {
     // Desactivar el botón durante la solicitud
     this.botonDesactivado = true;
   
-    this.usuarioService.create(this.nuevoUsu).subscribe(
+    const formData = this.updateForm.value;
+  
+    this.usuarioService.create(formData).subscribe(
       (response) => {
         // Éxito
-        console.log('usuario creada exitosamente:', response);
+        console.log('Usuario creado exitosamente:', response);
         // Resto de la lógica después de la creación exitosa
   
         // Cerrar la ventana después de guardar la jornada
@@ -110,17 +114,16 @@ export class CrearUsuarioComponent implements OnInit {
       },
       (error) => {
         // Manejo de errores
-        console.error('Error al crear el usu:', error);
+        console.error('Error al crear el usuario:', error);
         if (error.status === 401) {
           // Redirigir al usuario a la página de inicio de sesión
-          // Redirigir al usuario a la página de inicio de sesión
-          this['router'].navigate(['/login']);
+          this.router.navigate(['/login']);
         } else if (error.error && error.error.error) {
           // Muestra el mensaje de error específico del servidor al usuario
           alert(error.error.error);
         } else {
           // Muestra un mensaje de error genérico al usuario
-          alert('Error al crear la usu. Por favor, inténtelo de nuevo.');
+          alert('Error al crear el usuario. Por favor, inténtelo de nuevo.');
         }
   
         // Reactivar el botón después de un error
@@ -128,39 +131,7 @@ export class CrearUsuarioComponent implements OnInit {
       }
     );
   }
-  onSubmit() {
-    if (this.updateForm && this.updateForm.valid) {
-      const updatedUsu = this.updateForm.value;
-      updatedUsu.usu_id = this.usuario?.usu_id || 0;
 
-      console.log('Usuario ID seleccionado:', updatedUsu.usu_id);
-
-      if (!updatedUsu.usu_id) {
-        console.error('Error: ID de usuario no válido');
-        return;
-      }
-
-      this.usuarioService.updateUsuario(updatedUsu).subscribe(
-        data => {
-          console.log('Usuario actualizado con éxito:', data);
-          this.modalRef.hide();
-        },
-        error => {
-          console.error('Error al actualizar el usuario:', error);
-
-          if (error instanceof HttpErrorResponse && error.status === 401) {
-            this.router.navigate(['/login']);
-          } else if (error.error && error.error.error) {
-            alert(error.error.error);
-          } else {
-            alert('Error al actualizar el usuario. Por favor, inténtelo de nuevo.');
-          }
-
-          this.botonDesactivado = false;
-        }
-      );
-    }
-  }
 
   cancelar(): void {
     this.router.navigate(['/usuario']);
