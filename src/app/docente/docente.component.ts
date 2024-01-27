0bbef49255522df8a0d9762976c04ec56c498076
Damian2340842 <damianparedes842@gmail.com>
2024-01-27 12:17:09 -0500

    jjj
    hhh

--- src/app/docente/docente.component.ts
+++ src/app/docente/docente.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonaComponent } from '../persona/persona.component';
+import { TipoContratoComponent } from '../tipo-contrato/tipo-contrato.component';
+import { CargoComponent } from '../cargo/cargo.component';
+import { TituloComponent } from '../titulo/titulo.component';
+import { ListarPeriodoComponent } from '../periodos/listar-periodo.component';
+import { DocenteService } from './docente.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
+import { Docente } from './docente';
+import { GradoOcupacional } from '../grado-ocupacional/grado-ocupacional';
 
 @Component({
   selector: 'app-docente',
   templateUrl: './docente.component.html',
   styleUrls: ['./docente.component.css']
 })
-export class DocenteComponent {
+export class DocenteComponent implements OnInit{
+  persona: PersonaComponent | undefined; 
+tipo_con:TipoContratoComponent| undefined
+cargo:CargoComponent| undefined;
+titulo:TituloComponent| undefined;
+periodo:ListarPeriodoComponent| undefined;
+grado:GradoOcupacional| undefined;
+[x: string]: any;
+docentes: Docente[] = [];
+urlEndPoint_3: any;
+  http: any;
+  isLoading: boolean = true; // Nueva propiedad para rastrear si la carga está en progreso
+  docentesFiltradas: Docente[] = [];  // Nuevo array para las jornadas filtradas
+  todasLadocentes: Docente[] = [];
+  
+  modalRef: BsModalRef | undefined;
+    docente: Docente | undefined;
+constructor(private docente_service: DocenteService, private modalService: BsModalService) {}
 
+ngOnInit(): void {
+  this.cargarLista();
+  FormsModule;  }
+cargarLista(): void {
+  this.docente_service.getDocentes().subscribe(
+    docentes => {
+      this.docentes = docentes;
+      this.isLoading = false;
+      console.log('Docentes cargados correctamente:', docentes);
+      // Marcar la carga como completa después de recibir los docentes
+    },
+    error => {
+      console.error('Error al cargar los docentes:', error);
+      this.isLoading = false; // Marcar la carga como completa en caso de error
+    }
+  );
+}
+
+cargarDocente(docente_id: number): void {
+  this.docente_service.getdocentesId(docente_id).subscribe(
+    data => {
+      this.docente = data;
+      console.log(data); // Muestra la respuesta en la consola
+      this.eliminarUsu(this.docente.docente_id);  // Llamada a la función para abrir el modal
+    },
+    error => {
+      console.error(error);
+    }
+  );
+}
+eliminarUsu(docente_id: number): void {
+  if (confirm('¿Estás seguro de que deseas eliminar esta docente?')) {
+    // Llama al servicio para eliminar el rol
+    this.docente_service.deleteDocente(docente_id).subscribe(
+      data => {
+        console.log('usuario eliminado con éxito:', data);
+        // Aquí puedes realizar acciones adicionales después de la eliminación
+      },
+      error => {
+        console.error('Error al eliminar el persona:', error);
+        // Manejar el error según sea necesario
+      }
+      
+    );
+  }
+}
+textoBusqueda: string = '';
+usuMatchesSearch(docente: Docente): boolean {
+  return (docente.persona.per_primer_nombre.toLowerCase() + ' ' + docente.persona.per_apellido_paterno.toLowerCase()).includes(this.textoBusqueda.toLowerCase());
+}
+
+buscar(): void {
+  if (this.textoBusqueda.trim() !== '' ) {
+    this.docentes = this.docentes.filter((docente: Docente) => this.usuMatchesSearch(docente));
+  } else {
+    this.cargarLista(); // Vuelve a cargar todas las jornadas
+    }
+  }
 }
--- src/app/docente/docente.service.ts
+++ src/app/docente/docente.service.ts
@@ -1,9 +1,90 @@
 import { Injectable } from '@angular/core';
+import { Persona } from '../persona/persona';
+import { TipoContrato } from '../tipo-contrato/tipo-contrato';
+import { Cargo } from '../cargo/cargo';
+import { Titulo } from '../titulo/titulo';
+import { Periodos } from '../periodos/periodo';
+import { Docente } from './docente';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GradoOcupacional } from '../grado-ocupacional/grado-ocupacional';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocenteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/docente/listar';
+  private urlEndPoint_1: string = 'http://localhost:8080/api/docente/guardar';
+  private urlpersona: string = 'http://localhost:8080/api/personas/listar';
+  private urlcontrato: string = 'http://localhost:8080/api/tipocontratos/listar';
+  private urlcargo: string = 'http://localhost:8080/api/cargo/listar';
+  private urltitulo: string = 'http://localhost:8080/api/titulo/listar';
+  private urlGrado: string = 'http://localhost:8080/api/grado/listar';
 
-  constructor() { }
+  private urlperiodo: string = 'http://localhost:8080/api/periodos/listar';
+ docentes: Docente[] = [];
+  personas: Persona[] = [];
+contratos: TipoContrato[] = [];
+cargos: Cargo[] = [];
+titulos: Titulo[] = [];
+periodos: Periodos[] = [];
+grados:GradoOcupacional[]=[];
+private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json'});
+
+
+constructor(private http: HttpClient) { }
+getDocentes(): Observable<Docente[]> {
+  return this.http.get<Docente[]>(this.urlEndPoint);
+}
+create(docentes: Docente): Observable<Docente> {
+  return this.http.post<Docente>(this.urlEndPoint_1, docentes, {headers: this.httpHeaders})
+}
+getdocentesId(id:number):Observable<Docente> {
+  return this.http.get<Docente>(`${this.urlEndPoint_1}/${id}`)
+}
+deleteDocente(docente_id: number): Observable<Docente> {
+  const url = `http://localhost:8080/api/docente/eliminar/${docente_id}`; // Ajusta la URL según tu estructura
+   return this.http.delete<Docente>(url);
+}
+   updatedocente(docente: Docente): Observable<Docente> {
+    const url = `http://localhost:8080/api/docente/actualizar/${docente.docente_id}`;
+    console.log('URL de actualización:', url);
+    return this.http.put<Docente>(url, Docente);
+   }
+   getpers(): Observable<Persona[]> {
+    return this.http.get<Persona[]>(this.urlpersona);
+  }
+  getcontrato(): Observable<TipoContrato[]> {
+    return this.http.get<TipoContrato[]>(this.urlcontrato);
+  }
+  getcargo(): Observable<Cargo[]> {
+    return this.http.get<Cargo[]>(this.urlcargo);
+  }
+  gettitulo(): Observable<Titulo[]> {
+    return this.http.get<Titulo[]>(this.urltitulo);
+  }
+  getperiodos(): Observable<Periodos[]> {
+    return this.http.get<Periodos[]>(this.urlperiodo);
+  }
+  getgrado(): Observable<GradoOcupacional[]> {
+    return this.http.get<GradoOcupacional[]>(this.urlGrado);
+  }
+  cargarPersonas() {
+    this.getpers().subscribe(personas => this.personas = personas);
+  }
+  cargarcontrato() {
+    this.getcontrato().subscribe(contratos1 => this.contratos = contratos1);
+  }
+  cargarCargo() {
+    this.getcargo().subscribe(cargos => this.cargos = cargos);
+  }
+  cargarTitulos() {
+    this.gettitulo().subscribe(titulos => this.titulos = titulos);
+  }
+  cargarPeriodos() {
+    this.getperiodos().subscribe(periodos => this.periodos = periodos);
+  }
+  cargarGrados() {
+    this.getgrado().subscribe(grados => this.grados = grados);
+  }
 }
