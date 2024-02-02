64076f635847b5b49771a5680ebcc98b42f1fb93
ADRVDJ <adrian.cajamarca.est@tecazuay.edu.ec>
2024-02-01 23:59:38 -0500

    asd
    asd

--- src/app/persona/persona.service.ts
+++ src/app/persona/persona.service.ts
@@ -5,67 +5,66 @@ import { Persona } from './persona';
 import { filter, map } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 
-  export class PersonaService{
-  [x: string]: any;
-  
-    private urlEndPoint:string = 'http://localhost:8080/api/personas/listar'
-    private urlEndPoint_1:string = 'http://localhost:8080/api/personas/guardar'
+export class PersonaService {
 
-  
-    private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json'});
-    
-    constructor(private http:HttpClient) {}
-  
-      getPersona(): Observable<Persona[]> {
-   
-        return this.http.get<Persona[]>(this.urlEndPoint);
-      }
-  
-      create(persona: Persona): Observable<Persona> {
-        return this.http.post<Persona>(this.urlEndPoint_1, persona, {headers: this.httpHeaders})
-      }
-  
-      getpersonaid(id:number):Observable<Persona> {
-        return this.http.get<Persona>(`${this.urlEndPoint_1}/${id}`)
-      }
-  
-   //   deleteCliente(id: number): Observable<Cliente> {
-   //     return this.http.delete<Cliente>(`${this.urlEndPoint_2}/${id}`);
-   //   }
-   
-    deletePersona(per_id: number): Observable<Persona> {
-          const url = `http://localhost:8080/api/personas/eliminar/${per_id}`; // Ajusta la URL según tu estructura
-           return this.http.delete<Persona>(url);
-    }
-  
+  private urlEndPoint: string = 'http://localhost:8080/api/personas/listar'
+  private urlEndPoint_1: string = 'http://localhost:8080/api/personas/guardar'
 
- updatePersona(persona: Persona): Observable<Persona> {
-  const url = `http://localhost:8080/api/personas/actualizar/${persona.per_id}`;
-  console.log('URL de actualización:', url);
-  return this.http.put<Persona>(url, persona);
-}
-///
-getbypersona(cedula: string): Observable<Persona> {
-  return this.http.get<Persona[]>(this.urlEndPoint).pipe(
-    map(personas => personas.find(persona => persona.per_cedula === cedula) as Persona), 
-    filter(persona => !!persona) // Filtrar null o undefined
-  );
-}
-comboidpersona(nombre: string): Observable<boolean> {
-  return this.getPersona().pipe(
-    map(personas => personas.some(persona => persona.per_cedula === nombre))
-  );
-}
-getprsonaByName(nombre: string): Observable<Persona> {
-  return this.http.get<Persona[]>(this.urlEndPoint).pipe(
-    map(personas => personas.find(persona => persona.per_cedula === nombre) as Persona), 
-    filter(persona => !!persona) // Filtrar null o undefined
-  );
-  //
-}
+
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
+
+  getPersona(): Observable<Persona[]> {
+
+    return this.http.get<Persona[]>(this.urlEndPoint);
+  }
+
+  create(persona: Persona): Observable<Persona> {
+    return this.http.post<Persona>(this.urlEndPoint_1, persona, { headers: this.httpHeaders })
+  }
+
+  getpersonaid(id: number): Observable<Persona> {
+    return this.http.get<Persona>(`${this.urlEndPoint_1}/${id}`)
+  }
+
+  //   deleteCliente(id: number): Observable<Cliente> {
+  //     return this.http.delete<Cliente>(`${this.urlEndPoint_2}/${id}`);
+  //   }
+
+  deletePersona(per_id: number): Observable<Persona> {
+    const url = `http://localhost:8080/api/personas/eliminar/${per_id}`; // Ajusta la URL según tu estructura
+    return this.http.delete<Persona>(url);
+  }
+
+
+  updatePersona(persona: Persona): Observable<Persona> {
+    const url = `http://localhost:8080/api/personas/actualizar/${persona.per_id}`;
+    console.log('URL de actualización:', url);
+    return this.http.put<Persona>(url, persona);
+  }
+  ///
+  getbypersona(cedula: string): Observable<Persona> {
+    return this.http.get<Persona[]>(this.urlEndPoint).pipe(
+      map(personas => personas.find(persona => persona.per_cedula === cedula) as Persona),
+      filter(persona => !!persona) // Filtrar null o undefined
+    );
+  }
+  comboidpersona(nombre: string): Observable<boolean> {
+    return this.getPersona().pipe(
+      map(personas => personas.some(persona => persona.per_cedula === nombre))
+    );
+  }
+  getprsonaByName(nombre: string): Observable<Persona> {
+    return this.http.get<Persona[]>(this.urlEndPoint).pipe(
+      map(personas => personas.find(persona => persona.per_cedula === nombre) as Persona),
+      filter(persona => !!persona) // Filtrar null o undefined
+    );
+    //
+  }
 
 
 
