441af618ff548f7ca1047ce858a1fbeee714cbb0
¨Maikol <maikoltj11@hotmail.es>
2024-02-04 23:10:28 -0500

    gfgf
    ff

--- src/app/actividades-no-docente/actividad-no-docente.service.ts
+++ src/app/actividades-no-docente/actividad-no-docente.service.ts
@@ -5,81 +5,81 @@ import { Observable, filter, map } from 'rxjs';
 import { ActividadesNoDocente } from './actividades-no-docente';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ActividadNoDocenteService {
-
   docente: Docente[] = [];
-  
+
   //:Curso[]=[];
 
+  constructor(private http: HttpClient) {}
 
-  
-  constructor(private http: HttpClient) { }
+  private urlEndPoint: string =
+    'http://localhost:8080/api/actividades-no-docentes/listarActividadesNoDocentes';
 
-    private urlEndPoint:string = 'http://localhost:8080/api/actividades-no-docentes/listarActividadesNoDocentes'
+  private urlEndPoint_1: string =
+    'http://localhost:8080/api/actividades-no-docentes/guardarActividadNoDocente';
 
-    private urlEndPoint_1:string = 'http://localhost:8080/api/actividades-no-docentes/guardarActividadNoDocente'
+  //cdocentes
+  private urlDocente: string = 'http://localhost:8080/docente/listar';
+  private urlDocente1: string = 'http://localhost:8080/api/docente/listar';
 
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
- //cdocentes
- private urlDocente: string = 'http://localhost:8080/docente/listar';
- private urlDocente1: string = 'http://localhost:8080/api/docente/listar';
+  get(): Observable<ActividadesNoDocente[]> {
+    return this.http.get<ActividadesNoDocente[]>(this.urlEndPoint);
+  }
 
+  create(asignatura: ActividadesNoDocente): Observable<ActividadesNoDocente> {
+    return this.http.post<ActividadesNoDocente>(
+      this.urlEndPoint_1,
+      asignatura,
+      { headers: this.httpHeaders }
+    );
+  }
 
+  getactNoDocenteId(id: number): Observable<ActividadesNoDocente> {
+    return this.http.get<ActividadesNoDocente>(`${this.urlEndPoint_1}/${id}`);
+  }
 
-    private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json'});
-    
-  
-      get(): Observable<ActividadesNoDocente[]> {
-   
-        return this.http.get<ActividadesNoDocente[]>(this.urlEndPoint);
-      }
+  //   deleteCliente(id: number): Observable<Cliente> {
+  //     return this.http.delete<Cliente>(`${this.urlEndPoint_2}/${id}`);
+  //   }
 
-      create(asignatura: ActividadesNoDocente): Observable<ActividadesNoDocente> {
-        return this.http.post<ActividadesNoDocente>(this.urlEndPoint_1, asignatura, { headers: this.httpHeaders })
-      }
-  
-      getactNoDocenteId(id:number):Observable<ActividadesNoDocente> {
-        return this.http.get<ActividadesNoDocente>(`${this.urlEndPoint_1}/${id}`)
-      }
-  
-   //   deleteCliente(id: number): Observable<Cliente> {
-   //     return this.http.delete<Cliente>(`${this.urlEndPoint_2}/${id}`);
-   //   }
-   
-    deleteA(id: number): Observable<ActividadesNoDocente> {
-          const url = `http://localhost:8080/api/actividades-no-docentes/eliminarActividadNoDocente/${id}`; // Ajusta la URL según tu estructura
-           return this.http.delete<ActividadesNoDocente>(url);
-    }
-  
+  deleteA(id: number): Observable<ActividadesNoDocente> {
+    const url = `http://localhost:8080/api/actividades-no-docentes/eliminarActividadNoDocente/${id}`; // Ajusta la URL según tu estructura
+    return this.http.delete<ActividadesNoDocente>(url);
+  }
 
- updateAsignatura(actN: ActividadesNoDocente): Observable<ActividadesNoDocente> {
-  const url = `http://localhost:8080/api/actividades-no-docentes/actualizarActividadNoDocente/${actN.activiNoDoc_id}`;
-  console.log('URL de actualización:', url);
-  return this.http.put<ActividadesNoDocente>(url, actN);
-}
+  updateAsignatura(
+    actN: ActividadesNoDocente
+  ): Observable<ActividadesNoDocente> {
+    const url = `http://localhost:8080/api/actividades-no-docentes/actualizarActividadNoDocente/${actN.activinodoc_id}`;
+    console.log('URL de actualización:', url);
+    return this.http.put<ActividadesNoDocente>(url, actN);
+  }
 
+  //docente
+  getdoccente(): Observable<Docente[]> {
+    return this.http.get<Docente[]>(this.urlDocente1);
+  }
+  getDocentes(): Observable<Docente[]> {
+    return this.http.get<Docente[]>(this.urlDocente);
+  }
 
+  cargarDocentes() {
+    this.getdoccente().subscribe((doc) => (this.docente = doc));
+  }
 
-//docente
-getdoccente(): Observable<Docente[]> {
-  return this.http.get<Docente[]>(this.urlDocente1);
-}
-getDocentes(): Observable<Docente[]> {
-  return this.http.get<Docente[]>(this.urlDocente);
-}
-
-cargarDocentes() {
-  this.getdoccente().subscribe(doc => this.docente = doc);
-}
-
-
-getActNoDocenteByName(nombre: string): Observable<ActividadesNoDocente> {
-  return this.http.get<ActividadesNoDocente[]>(this.urlEndPoint).pipe(
-    map(actNo => actNo.find(actNoDoc => actNoDoc.actiNoDoc_nombre === nombre) as ActividadesNoDocente),
-    filter(actNoDoc => !!actNoDoc) // Filtrar null o undefined
-  );
-}
-
+  getActNoDocenteByName(nombre: string): Observable<ActividadesNoDocente> {
+    return this.http.get<ActividadesNoDocente[]>(this.urlEndPoint).pipe(
+      map(
+        (actNo) =>
+          actNo.find(
+            (actNoDoc) => actNoDoc.activinodoc_nombre === nombre
+          ) as ActividadesNoDocente
+      ),
+      filter((actNoDoc) => !!actNoDoc) // Filtrar null o undefined
+    );
+  }
 }
